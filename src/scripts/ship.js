167bfaedd40d45be13e01c863c61bed94aa4271b
Daniel <daniel.moderiano@outlook.com>
2021-08-17 13:08:02 +0930

    Updated JS modular file structure for multiple factories

---
+++ src/scripts/ship.js
@@ -0,0 +1,20 @@
+function Ship(length) {
+  if (length < 2 || length > 5) {
+    throw new Error('Invalid ship length, must be between 2 and 5 inclusive');
+  }
+
+  const hits = [];
+
+  // Fill this array with position occupied by ship. Can compare with hits later on
+  const position = [];
+
+  const hit = (cell) => {
+    hits.push(cell);
+  };
+
+  const isSunk = () => hits.length === length;
+
+  return { length, hit, isSunk };
+}
+
+export default Ship;
--- src/ship.test.js
+++ src/ship.test.js
@@ -1,37 +1,32 @@
-import createShip from './index.js';
+import Ship from './scripts/ship.js';
 
 test('createShip returns ship object correct length property', () => {
-  expect(createShip(5).length).toBe(5);
+  expect(Ship(5).length).toBe(5);
 });
 
 test('createShip throws error for lengths < 2 and > 5', () => {
-  expect(() => (createShip(0))).toThrow('Invalid ship length, must be between 2 and 5 inclusive');
+  expect(() => (Ship(0))).toThrow('Invalid ship length, must be between 2 and 5 inclusive');
 });
 
 test('isSunk returns false on freshly created ship with no hits', () => {
-  const testShip = createShip(3);
+  const testShip = Ship(3);
   expect(testShip.isSunk()).toBe(false);
 });
 
 test('isSunk returns false on ship that is partially hit', () => {
-  const testShip = createShip(3);
+  const testShip = Ship(3);
   testShip.hit(1);
   testShip.hit(2);
   expect(testShip.isSunk()).toBe(false);
 });
 
 test('isSunk returns true on ship hit appropriate number of times', () => {
-  const testShip = createShip(3);
+  const testShip = Ship(3);
   testShip.hit(1);
   testShip.hit(2);
   testShip.hit(3);
   expect(testShip.isSunk()).toBe(true);
 });
 
-// describe('Family of tests requiring pre-made ships with hits', () => {
-//   beforeEach(() => {
-//     const testShip = createShip(2);
-//     testShip
-//   });
-// });
+
 
