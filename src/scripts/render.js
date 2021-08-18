02a9647fb23d1158a00649e4b1234cf21fbe37d7
Daniel <daniel.moderiano@outlook.com>
2021-08-18 16:36:57 +0930

    Added classes and data-coordinates for dynamically generated tables

--- src/scripts/render.js
+++ src/scripts/render.js
@@ -1,12 +1,19 @@
 const tables = document.querySelectorAll('.board__table');
 
 function createTable(table) {
+
+  let count = 0;
+
   for (let i = 0; i < 10; i++) {
     const tr = document.createElement('tr');
+    tr.classList.add('board__row');
     table.appendChild(tr);
     for (let j = 0; j < 10; j++) {
       const td = document.createElement('td');
+      td.classList.add('board__cell');
+      td.dataset.coordinate = count;
       tr.appendChild(td);
+      count += 1;
     }
   }
 }
